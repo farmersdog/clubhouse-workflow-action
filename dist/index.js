0ad5d5819f4f52d83f14ec1813427dd103aa7b78
Matt Kuritz <mkuritz@thefarmersdog.com>
2022-11-20 23:32:13 -0500

    package

--- dist/index.js
+++ dist/index.js
@@ -16881,6 +16881,7 @@ async function updateStory(storyWithEndStateId) {
         storyWithEndStateId.storyId,
         params
     );
+    core.debug('\n full update story response: \n \n' + JSON.stringify(updatedStory));
     if (updatedStory.workflow_state_id !== storyWithEndStateId.endStateId) {
         throw new Error(
             `Tranistion failed for story ${storyWithEndStateId.storyId}`
@@ -16938,6 +16939,7 @@ async function releaseStories(
     );
     core.debug('\n stories with end states: \n \n' + JSON.stringify(storiesWithEndStateIds));
     const updatedStoryNames = await updateStories(storiesWithEndStateIds);
+    core.debug('\n updated story names: \n \n' + JSON.stringify(updatedStoryNames));
     return updatedStoryNames;
 }
 
@@ -16966,6 +16968,7 @@ async function transitionStories(
     );
     core.debug('\n stories with end states: \n \n' + JSON.stringify(storiesWithEndStateIds));
     const updatedStoryNames = await updateStories(storiesWithEndStateIds);
+    core.debug('\n updated story names: \n \n' + JSON.stringify(updatedStoryNames));
     return updatedStoryNames;
 }
 
